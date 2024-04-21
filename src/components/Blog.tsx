5264f4a2c82515b660f177f628ea735454176898
imran <zunaeede@gmail.com>
2024-04-21 14:58:13 +0600

    react_blog - change some hover color.

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -7,14 +7,14 @@ const Blog = (props: blogTypeprops) => {
   const { id, title, desc } = props.blog;
   return (
     <>
-      <article className="bg-indigo-200 text-center p-4 hover:bg-slate-400">
+      <article className="bg-indigo-200 text-center p-4 hover:bg-slate-300">
         <h3 className="text-xl p-1">{title}</h3>
         <h3 className="p-2">{desc}</h3>
         <button
           onClick={() => {
             props.handleDeleteBlog(id);
           }}
-        className="bg-red-500 p-1 rounded hover:bg-red-700">
+        className="bg-red-500 p-1 rounded hover:bg-red-600">
           Delete
         </button>
       </article>
